f5d9e6fcd6a0ae0870e2eb0fd55ea89c826dba27
Dayellowthing <146721148+Dayellowthing@users.noreply.github.com>
2026-05-12 23:36:49 +0700

    Update Rook.java
    Implemented icon, onMoveCommand and getMovableSquares methods

--- src/main/java/com/Logic/pieces/Rook.java
+++ src/main/java/com/Logic/pieces/Rook.java
@@ -1,11 +1,15 @@
 package com.Logic.pieces;
 
 import com.Logic.Color;
+import com.Logic.Direction;
 import com.Logic.GameState;
 import com.Logic.Piece;
 import com.Logic.Position;
+import com.Logic.Vector;
 
 public class Rook extends Piece {
+    private static final Direction[] Directions = {Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST};
+    
     public Rook(Color color, Position position) {
         super(color, position);
     }
@@ -17,12 +21,41 @@ public class Rook extends Piece {
 
     @Override
     protected void onMoveCommand(GameState state, Position to) {
-        // TODO: Implement bishop movement rules
+        String[][] movable = getMovableSquares(state);
+        if (movable[to.row()][to.col()] != null) {
+            if (state.hasEnemy(to, color)) {
+                state.capture(to);
+            }
+            state.move(position, to);
+            state.passControl();
     }
 
     @Override
     protected String[][] getMovableSquares(GameState state) {
-        // TODO: Return possible squares this bishop can move to
-        return new String[8][8];
+        String[][] moves = new String[8][8];
+
+        for (Direction d : Directions) {
+            Vector step = d.unitVector();
+            Vector newPos = position.toVector().add(step);
+
+            while (newPos.isInBounds()) {
+                Position pos = newPos.toPosition();
+                BoardPiece curr = state.getSquare(pos);
+
+                if (curr == null) {
+                    moves[pos.row()][pos.col()] = "greensquare.png";
+                }
+                else {
+                    if (curr.color() != color) {
+                        moves[pos.row()][pos.col()] = "redsquare.png";
+                    }
+                    break;
+                }
+
+                curr = curr.add(step);
+            }
+        }
+
+        return moves;
     }
 }
